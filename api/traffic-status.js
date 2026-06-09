dedfb577d20427bc082835e021ca06dcfdc45d9b
ech185 <75432964+ech185@users.noreply.github.com>
2026-06-08 20:44:01 -0400

    Update traffic-status.js

--- api/traffic-status.js
+++ api/traffic-status.js
@@ -1,10 +1,12 @@
 export default function handler(req, res) {
+  res.setHeader('Cache-Control', 'no-store');
+
   res.status(200).json({
-metlife: "traffic-heavy",
-fortlee: "traffic-heavy",
-ewr: "traffic-heavy",
-secaucus: "traffic-heavy",
-timessquare: "traffic-heavy"
+    metlife: "traffic-heavy",
+    fortlee: "traffic-heavy",
+    ewr: "traffic-heavy",
+    secaucus: "traffic-heavy",
+    timessquare: "traffic-heavy"
   });
 }
 
